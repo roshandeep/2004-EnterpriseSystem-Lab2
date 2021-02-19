9ea095b2e311d1b429411976f7a4c085f48d6a12
roshandeep810 <roshandeep810@gmail.com>
2021-02-19 06:15:04 +0000

    Lab 2 Commit from VM Windows server RDP

--- Add_Num.py
+++ Add_Num.py
@@ -1,8 +1,12 @@
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
-# Add two numbers
 sum = float(num1) + float(num2)
-
-# Display the sum
+diff = float(num1) - float(num2)
+product = float(num1) * float(num2)
+div = float(num1) / float(num2)
+ 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The difference of {0} and {1} is {2}'.format(num1, num2, diff))
+print('The product of {0} and {1} is {2}'.format(num1, num2, product))
+print('The Quotient of {0} and {1} is {2}'.format(num1, num2, div))
